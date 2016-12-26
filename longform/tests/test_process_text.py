ef1d52b6454912d66be2508627465cf527f67f0f
Dmitry Groshev <lambdadmitry@gmail.com>
2016-12-26 18:19:15 +0300

    rework tests to Django ones, minimize install requirements

--- longform/tests/test_process_text.py
+++ longform/tests/test_process_text.py
@@ -1,3 +1,5 @@
+from django.test import TestCase
+
 from .. import helpers
 
 
@@ -8,58 +10,60 @@ def _strip_special_chars(result):
             .strip())
 
 
-def test_safety():
-    text = """
+class ProcessTextTestCase(TestCase):
+    def test_safety(self):
+        text = """
 I am a hacker. <script>alert("hello there!")</script>
-    """
-    assert "<script>" not in helpers.process_text(text)
-    assert "<script>" in helpers.process_text(text, sanitize=False)
+        """
+        self.assertNotIn("<script>", helpers.process_text(text))
+        self.assertIn("<script>", helpers.process_text(text, sanitize=False))
 
 
-def test_p_stripping():
-    text = "# The **big** thing"
-    result = _strip_special_chars(helpers.process_text(text))
-    assert result == "<h1>The <strong>big</strong> thing</h1>"
+    def test_p_stripping(self):
+        text = "# The **big** thing"
+        result = _strip_special_chars(helpers.process_text(text))
+        self.assertEqual(result, "<h1>The <strong>big</strong> thing</h1>")
 
 
-def test_linkification():
-    text = """
+    def test_linkification(self):
+        text = """
 There is a link https://google.com somewhere here
 
 And we can have [markdown link](https://stripe.com)
-    """
-    result = _strip_special_chars(helpers.process_text(text))
-    assert "href=https://google.com" in result
-    assert ">https://google.com</a>" in result
-    assert "href=https://stripe.com" in result
-    assert ">markdown link</a>" in result
+        """
+        result = _strip_special_chars(helpers.process_text(text))
+
+        self.assertIn("href=https://google.com", result)
+        self.assertIn(">https://google.com</a>", result)
+        self.assertIn("href=https://stripe.com", result)
+        self.assertIn(">markdown link</a>", result)
 
 
-def test_smartypants():
-    text = "Look... This is so smart --- you can have this, too"
-    result = _strip_special_chars(helpers.process_text(text))
-    assert "…" in result
-    assert "—" in result
+    def test_smartypants(self):
+        text = "Look... This is so smart --- you can have this, too"
+        result = _strip_special_chars(helpers.process_text(text))
+        self.assertIn("…", result)
+        self.assertIn("—", result)
 
 
-def test_hyphenation():
-    text = "Strange word: impersonation"
-    result = helpers.process_text(text)
-    assert "im\xadper\xadson\xadation" in result
+    def test_hyphenation(self):
+        text = "Strange word: impersonation"
+        result = helpers.process_text(text)
+        self.assertIn("im\xadper\xadson\xadation", result)
 
 
-def test_widont_oneword():
-    text = """
+    def test_widont_oneword(self):
+        text = """
 1. Study of the world around
 2. Knowledge
 3. The Earth
 """
-    required_result = """
+        required_result = """
 <ol>
 <li>Study of the world\xa0around</li>
 <li>Knowl\xadedge</li>
 <li>The\xa0Earth</li>
 </ol>
 """
-    result = helpers.process_text(text)
-    assert result.strip() == required_result.strip()
+        result = helpers.process_text(text)
+        self.assertEqual(result.strip(), required_result.strip())
